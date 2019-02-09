41a5c46fa75aeb67f9b8b9ab7af1460e1b681082
Andrew Ocampo <aocampo2862@gmail.com>
2019-02-09 13:59:31 -0600

    Ready for code review

--- NewVisionPractice/src/main/java/frc/robot/Robot.java
+++ NewVisionPractice/src/main/java/frc/robot/Robot.java
@@ -1,53 +1,24 @@
 package frc.robot;
 
-import javax.lang.model.util.ElementScanner6;
-
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//----Imports----\\
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.VideoMode;
-import edu.wpi.cscore.VideoSource;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.shuffleboard.SendableCameraWrapper;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 
   //----Object Declarations----\\
+
+  // xbox controller used for controlling the robot
   Joystick stick = new Joystick(0);
 
-  //----Variable Declarations----\\
-  double horizontalSpeed = 0;
-  double stickX = 0;
-  double stickY = 0;
-
-  //----Constants----\\
-  public final double DEFAULT_HORIZONTAL_SPEED = -0.01;
-  public final double DEFAULT_LIMELIGHT_VALUE = 0.1234;
-
-  // Controller constants
-  public final int A_BUTTON = 1;
-  public final int Y_BUTTON = 4;
-  public final int LEFT_Y_AXIS = 1;
-  public final int RIGHT_X_AXIS = 4;
-
-  // real-world measurements
-  public final double DISTANCE_FROM_TARGET = 40.0;
-  public final double CAM_HEIGHT = 14.375;
-  public final double TARGET_HEIGHT = 27.5;
-  public final double HEIGHT = TARGET_HEIGHT - CAM_HEIGHT;
-  public final double INITIAL_ANGLE = Math.atan(HEIGHT/DISTANCE_FROM_TARGET);
-  public final double ANGLE_ERROR = 0.45;
+  // NetworkTable object declaration used to get values from the limelight network table
+  NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
 
   // Spark motor controllers
   CANSparkMax leftMotor = new CANSparkMax(1, MotorType.kBrushless);
@@ -57,13 +28,57 @@ public class Robot extends TimedRobot {
   CANSparkMax rightFollow1 = new CANSparkMax(5, MotorType.kBrushless);
   CANSparkMax rightFollow2 = new CANSparkMax(6, MotorType.kBrushless);
 
+  //----Constants----\\
+  private final double DEFAULT_HORIZONTAL_SPEED = -0.01;
+  private final double HORIZONTAL_ANGLE_THRESHOLD = 0.3;
+
+  // default value that the limelight will return if no targets are found
+  private final double DEFAULT_LIMELIGHT_VALUE = 0.1234;
+
+  // Controller constants
+  private final int A_BUTTON = 1;
+  private final int LEFT_Y_AXIS = 1;
+  private final int RIGHT_X_AXIS = 4;
+  private final double JOYSTICK_DEADBAND = 0.000124;
+
+  private final int CAMERA1_WIDTH = 320;
+  private final int CAMERA1_HEIGHT = 240;
+  private final int CAMERA1_FPS = 30;
+  private final int CAMERA2_WIDTH = 160;
+  private final int CAMERA2_HEIGHT = 120;
+  private final int CAMERA2_FPS = 30;
+
+  //----Variable Declarations----\\
+
+  // get the angle of the horizontal offset between the camera's crosshair and the target's crosshair
+  NetworkTableEntry tx = limelightTable.getEntry("tx");
+
+  // typecast the NetworkTable data into a double
+  // getDouble() will return a default value if no value is found
+  double horizontalAngle = tx.getDouble(DEFAULT_LIMELIGHT_VALUE);
+
+  // left and right speeds for the drivetrain 
+  double leftMovement = 0.0;
+  double rightMovement = 0.0;
+
+  // speed of the robot in the forward and backward directions
+  double distanceSpeed = 0;
+
+  // speed of the robot in the left and right directions
+  double horizontalSpeed = 0;
+
+  // values of the left joystick's x-axis and the right joystick's y-axis
+  double stickX = 0;
+  double stickY = 0;
+
   @Override
   public void robotInit() {
+
     /* set the limelight and USB camera to picture-in-picture mode,
        which means the limelight's camera feed is shown in the
        bottom right corner of the USB camera's feed
     */
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("stream").setNumber(2);
+    limelightTable.getEntry("stream").setNumber(2);
 
     // configure 2 motors on each side of the robot to follow the main motor on each side
     leftFollow1.follow(leftMotor);
@@ -71,18 +86,10 @@ public class Robot extends TimedRobot {
     leftFollow2.follow(leftMotor);
     rightFollow2.follow(rightMotor);
 
+    // begin sending data to the shuffleboard
     Shuffleboard.startRecording();
 
-    /*
-    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-		camera.setResolution(800, 600);
-		camera.setFPS(30);
-    */
-
-    //CameraServer.getInstance().startAutomaticCapture();
-
-    //Shuffleboard.getTab("LiveWindow").add("Video Stream", SendableCameraWrapper.wrap(CameraServer.getInstance().putVideo("Cam", 6000, 6000)));
-
+    // initialize USB camera objects and begin sending their video streams to shuffleboard
     startCapture();
   }
 
@@ -104,90 +111,89 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    // NetworkTable object declaration used to get values from the limelight network table
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-
-
-    //read values periodically, return 0.1234 as the default value if nothing is found
-    double horizontalAngle = tx.getDouble(DEFAULT_LIMELIGHT_VALUE);
-    double verticalAngle = ty.getDouble(DEFAULT_LIMELIGHT_VALUE);
-
-    double leftMovement = 0.0;
-    double rightMovement = 0.0;
-    double distance = 0;
-    double distanceSpeed = 0;
-
+    // reset the horizontal and distance speeds every time the code runs
+    // this will prevent previous leftover values from moving the motors
     horizontalSpeed = 0;
     distanceSpeed = 0;
 
     // checks if the A button is currently being pressed, returns a boolean
 	  if(stick.getRawButton(A_BUTTON)) {
-      // reset the horizontal and distance speeds every time the code runs
-      // this will prevent previous leftover values from moving the motors
-      horizontalSpeed = 0;
-      distanceSpeed = 0;
 
-      // if the A button is currently pressed, 
-      NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1 + (2 * (stick.getRawButton(A_BUTTON) ? 1 : 0)));
+      /* if the A button is currently pressed, turn on the limelight's LEDs,
+         and if the A button is not pressed, turn off the LEDs
 
-      // rotate the robot towards the target if horizontal angle is greater than 8 degrees on either side of the target
-      rotate(horizontalAngle, 3.0);
+         getEntry("ledMode") is used to get the LedMode property from the NetworkTable
+         setNumber() is used to set the state of the LedMode property (the integer 1 sets the LEDs off, 3 sets the LEDs on)
+         
+         ?: is a terenary operator in Java, it is basically a one-line if-else statement
+         To use it, give it a boolean variable, the do ?. Then after that, you can use it as
+         an if-else statement. 
+
+         stick.getRawButton(A_BUTTON) ? 1 : 0
+         In this example, we use stick.getRawButton(A_BUTTON) as our boolean variable
+         if stick.getRawButton(A_BUTTON) is down (value is true), then it will return 1
+         if stick.getRawButton(A_BUTTON) is up (value is false), then it will return 0
+
+         With the 0 and 1 then, we use it to do some math. 
+         When the stick.getRawButton(A_BUTTON) is down (true), it returns 1
+         1 + (2 * 1) = 3. 3 is sent into .setNumber(), which turns the LEDs on
+      */
+      limelightTable.getEntry("ledMode").setNumber(1 + (2 * (stick.getRawButton(A_BUTTON) ? 1 : 0)));
+
+      // rotate the robot towards the target if horizontal angle is greater than the horizontal angle threshold on either side of the target
+      rotate(horizontalAngle);
     }
 
     // Cubing values to create smoother function
     stickX = -Math.pow(stick.getRawAxis(LEFT_Y_AXIS), 3);
     stickY = Math.pow(stick.getRawAxis(RIGHT_X_AXIS), 3);
 
-    // Joystick deadband
-    if(Math.abs(stickX) > 0.000124) {
+    // manual drive controls
+    if(Math.abs(stickX) > JOYSTICK_DEADBAND) {
       distanceSpeed = stickX;
     }
-    if(Math.abs(stickY) > 0.000124) {
+    if(Math.abs(stickY) > JOYSTICK_DEADBAND) {
       horizontalSpeed = stickY;
     }
 
-    // left and right speeds
+    // left and right speeds of the drivetrain
     leftMovement = distanceSpeed + horizontalSpeed;
     rightMovement = distanceSpeed - horizontalSpeed;
 
-    // run the robot
+    // run the robot based on the left and right speeds of the drive train
     runAt(-leftMovement, rightMovement);
   }
 
   @Override
   public void testPeriodic() {
+
   }
 
-  private static double degreeToRadian(double degree)
-  {
-      return Math.toRadians(degree);
-  } 
-
    // drive the robot
-   public void runAt(double leftSpeed, double rightSpeed) {
-        
+   private void runAt(double leftSpeed, double rightSpeed) {      
     leftMotor.set(leftSpeed);
     rightMotor.set(rightSpeed);
    }
 
-   // rotate the robot based on horizontal angle offset
-   public void rotate(double xAngle, double angleThreshold) {
-      if(Math.abs(xAngle) > angleThreshold) 
+   // rotate the robot based on horizontal angle offset between the camera's crosshair and the target's crosshair
+   private void rotate(double xAngle) {
+      if(Math.abs(xAngle) > HORIZONTAL_ANGLE_THRESHOLD) 
         horizontalSpeed = DEFAULT_HORIZONTAL_SPEED * (-xAngle / 1.5);
    }
 
-   public static void startCapture() {
+   // begin capturing video from two USB cameras and send their video streams to shuffleboard
+   private void startCapture() {
+    // creates a thread which runs concurrently with the program
 		new Thread(() -> {
-			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
-			camera.setResolution(320, 240);
-      camera.setFPS(30);
+      // Instantiate the USB cameras and begin capturing their video streams
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
       UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
-			camera2.setResolution(160, 120);
-      camera2.setFPS(30);
-      //camera.setVideoMode(VideoMode.PixelFormat.kRGB565, 320, 240, 30);
+
+      // set the cameras' reolutions and FPS
+			camera.setResolution(CAMERA1_WIDTH, CAMERA1_HEIGHT);
+      camera.setFPS(CAMERA1_FPS);
+			camera2.setResolution(CAMERA2_WIDTH, CAMERA2_HEIGHT);
+      camera2.setFPS(CAMERA2_FPS);
 		}).start();
 	}
 }
